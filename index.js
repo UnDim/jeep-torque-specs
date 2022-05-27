b9ba74d904b143abc7528f9e8f4b6d79cdf7576c
Shannon Odam <shannon.odam@icloud.com>
2022-05-27 01:41:45 -0400

    test db connection

--- index.js
+++ index.js
@@ -21,10 +21,12 @@ express()
       const result = await client.query('select cat.category, ts.description, ts.n_m, ts.ft_lbs, ts.in_lbs, ts.comments, mod.model from torque_specs as ts join categories as cat on ts.category_id = cat.id join models as mod on ts.model_id = mod.id;');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
+      res.send('/pages/body', results);
       client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
+      
     }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
